fd6724dcba4e6f071e104253252087c0e0e9f774
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2020-01-01 20:45:54 +0000

    wiring of logout function

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
-# Create your views here.
+# log in page for user
 def login(request):
     return render(request, 'accounts/login.html')
+
+# logout page for user
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('index'))
