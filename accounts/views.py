7071d6b3d245429903b62944bbf1fd54548e21f5
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2020-01-01 22:19:15 +0000

    create navbar partial and login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 # log in page for user
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(
+                username=request.POST['username'],
+                password=request.POST['password'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+
+            else:
+                login_form.add_error(None, "Your username or password is incorrect!")
+    else:
+        login_form = UserLoginForm()
+
+    context =   {"login_form" : login_form}
+    return render(request, 'accounts/login.html', context)
 
 # logout page for user
 def logout(request):
