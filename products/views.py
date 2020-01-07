3bad307209e57ad21f0da7eaf7aff8d15a52c13d
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2020-01-07 20:16:25 +0000

    add pagination to shop page

--- products/views.py
+++ products/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
+
+""" This is the all products shop page """
 def products(request):
     products = Product.objects.all()
-    context = { 'products': products }
+
+    # Pagination on all products page
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+
+    context = { 'products': paged_products, 'products_all': products }
     return render(request, 'products/products.html', context)
 
+""" This is the for singular product information page """
 def product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     category = Category.objects.all().filter(name=True)
