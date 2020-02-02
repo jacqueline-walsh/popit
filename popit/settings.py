2ba3f667eea7a1caf919d257a777cb89aeb4325d
Jacqueline Walsh <jacquelinewalsh@jacquelines-mbp.home>
2020-02-02 19:16:12 +0000

    add checkout

--- popit/settings.py
+++ popit/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
-# import env
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'category',
     'cart',
     'search',
+    'checkout',
 ]
 
 MIDDLEWARE = [
