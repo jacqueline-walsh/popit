686b89dc5830a44fea94a7b1a8578e690a1dd41f
Jacqueline Walsh <jacquelinewalsh@jacquelines-mbp.home>
2020-02-06 17:19:22 +0000

    checkout build

--- popit/urls.py
+++ popit/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('search/', include('search.urls')),
+    path('checkout/', include('checkout.urls')),
+
 
 ]
